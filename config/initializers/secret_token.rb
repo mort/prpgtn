46349cb87c2bff2d735c400312d76d035532e93f
mort <manuel.gonzalez.noriega@gmai.com>
2013-01-06 18:09:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sputnik::Application.config.secret_token = 'a68dd83741b698a9aca8d5d3ed3c3170f73240bbb7a4329b844e05bf0c123343d6bc0b700c025b89c0c0f0e0999bcba9ca1847c2b79a20870a0d511d26026fd6'
