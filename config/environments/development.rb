ce12698e343eb928f311e2b0956343bbd6a8c4bb
mort <manuel.gonzalez.noriega@gmail.com>
2014-05-05 19:44:49 +0200

    Preventing email sending on dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,8 @@ Peach::Application.configure do
   config.action_controller.asset_host = 'http://localhost:3000' # Locally
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
+  config.action_mailer.perform_deliveries = false
     
   config.eager_load = false  
 
